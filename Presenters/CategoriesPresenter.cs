cfeb8f8a9c2c3ab9538addb7116030ac0efc2e64
Jhon Steven Ramirez <stevenramirez1115@gmail.com>
2023-10-21 23:21:56 -0500

    categires_Presenters

--- Presenters/CategoriesPresenter.cs
+++ Presenters/CategoriesPresenter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Supermarket_mvp1.Models;
+using Supermarket_mvp1.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,78 @@ namespace Supermarket_mvp1.Presenters
 {
     internal class CategoriesPresenter
     {
+
+        private ICategoriesView view;
+        private ICategoriesRepository repository;
+        private BindingSource categoriesBindingSource;
+        private IEnumerable<PayModeModel> categoriesList;
+
+        public CategoriesPresenter(ICategoriesView view, ICategoriesRepository repository)
+        {
+            this.categoriesBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchCategorie;
+            this.view.AddNewEvent += AddNewCategorie;
+            this.view.EditEvent += LoadSelectCategorie;
+            this.view.DeleteEvent += DeleteSelectedCategorie;
+            this.view.SaveEvent += SaveCategorie;
+            this.view.CancelEvent += CancelCategorie;
+
+            this.view.SetPayModeListBildingSource(categoriesBindingSource);
+
+            loadAllCategoriesList();
+
+            this.view.Show();
+
+        }
+
+        private void loadAllCategoriesList()
+        {
+            categoriesList = repository.GetAll();
+            categoriesBindingSource.DataSource = categoriesList;
+
+        }
+
+        private void CancelCategorie(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveCategorie(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteSelectedCategorie(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectCategorie(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNewCategorie(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SearchCategorie(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                categoriesList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                categoriesList = repository.GetAll();
+            }
+            categoriesBindingSource.DataSource = categoriesList;
+        }
     }
 }
--- Views/CategoriesView.cs
+++ Views/CategoriesView.cs
@@ -8,13 +8,93 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Supermarket_mvp1.Views
 {
-    public partial class CategoriesView : Form
+    public partial class CategoriesView : Form, ICategoriesView
     {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string messaje;
         public CategoriesView()
         {
             InitializeComponent();
+            AssociateAndRaiseViewEvents();
+            tabControl1.TabPages.Remove(tabCategorieDetail);
+
+        }
+
+        private void AssociateAndRaiseViewEvents()
+        {
+            BtnSearch.Click += delegate { SearchEvent?.Invoke(this, EventArgs.Empty); };
+
+            TxtSearch.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    SearchEvent?.Invoke(this, EventArgs.Empty);
+                }
+            };
+
+            BtnNew.Click += delegate
+            {
+                AddNewEvent?.Invoke(this, EventArgs.Empty);
+
+                tabControl1.TabPages.Remove(tabCategorieList);
+                tabControl1.TabPages.Add(tabCategorieDetail);
+                tabCategorieDetail.Text = "Add New Pay Mode";
+
+            };
+            BtnEdit.Click += delegate
+            {
+                EditEvent?.Invoke(this, EventArgs.Empty);
+
+                tabControl1.TabPages.Remove(tabCategorieList);
+                tabControl1.TabPages.Add(tabCategorieDetail);
+                tabCategorieDetail.Text = "Edit Pay Mode";
+
+            };
+        }
+
+        public string CategorieId {
+            get { return TxtCategorieModeId.Text; }
+            set { TxtCategorieModeId.Text = value; }
+        }
+        public string CategorieName {
+            get { return TxtPayCategorieName.Text; }
+            set { TxtPayCategorieName.Text = value; }
+        }
+        public string PayModeObservation {
+            get { return TxtCategorieObservation.Text; }
+            set { TxtCategorieObservation.Text = value; }
+        }
+        public string SearchValue {
+            get { return TxtSearch.Text; }
+            set { TxtSearch.Text = value; }
+        }
+        public bool IsEdit {
+            get { return isEdit; }
+            set { isEdit = value; }
+        }
+        public bool IsSuccessful {
+            get { return isSuccessful; }
+            set { isSuccessful = value; }
+        }
+        public string Message {
+            get { return messaje; }
+            set { messaje = value; }
+        }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetPayModeListBildingSource(BindingSource CategoriesList)
+        {
+            DgCategorie.DataSource = CategoriesList;
         }
     }
 }
